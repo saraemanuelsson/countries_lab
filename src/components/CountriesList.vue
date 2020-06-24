fa849ec94dc2d3981de56ac0e5c46768d4337cee
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-06-24 15:38:26 +0100

    Added html for CountriesList

---
+++ src/components/CountriesList.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+    <div>
+        <ul>
+            <list-item v-for="(country, index) in country" :country="country" :key="index"></list-item>
+        </ul>
+    </div>
+  
+</template>
+
+<script>
+import ListItem from "./ListItem.vue";
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
